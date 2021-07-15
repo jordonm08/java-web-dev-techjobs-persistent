5b91c5db6a6970383480d990e0dfdc67888a3dc4
jordonm08 <jorodnm08@gmail.com>
2021-07-15 11:58:00 -0500

    fixed the list

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
@@ -1,7 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
 import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,12 @@ public class ListController {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
     static HashMap<String, String> columnChoices = new HashMap<>();
 
     public ListController () {
@@ -26,12 +34,15 @@ public class ListController {
         columnChoices.put("all", "All");
         columnChoices.put("employer", "Employer");
         columnChoices.put("skill", "Skill");
+        columnChoices.put("skills", "Skills");
 
     }
 
     @RequestMapping("")
     public String list(Model model) {
-
+        model.addAttribute("all", "viewAll");
+        model.addAttribute("employers",employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         return "list";
     }
 
@@ -43,10 +54,11 @@ public class ListController {
             model.addAttribute("title", "All Jobs");
         } else {
             jobs = JobData.findByColumnAndValue(column, value, jobRepository.findAll());
+            System.out.println(jobs);
             model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         }
         model.addAttribute("jobs", jobs);
-
+        //System.out.println(jobs);
         return "list-jobs";
     }
 }
